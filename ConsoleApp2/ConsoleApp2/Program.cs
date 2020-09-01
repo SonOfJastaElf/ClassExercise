ea485363fd455868675a59cc8fd99996bea27d2f
Brian Henderson <henderson.brian82@gmail.com>
2020-09-01 13:00:55 -0400

    Added a simple class exercise in C#

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassExercise
+{
+    class Program {
+
+        static void Main(string[] args)
+        {
+            Car firstVehicle = new Car();
+            firstVehicle.Make = "Ford";
+            firstVehicle.Model = "Fusion";
+            firstVehicle.Year = 2012;
+            Console.WriteLine(firstVehicle.Make);
+            Console.WriteLine(firstVehicle.Model);
+            Console.WriteLine(firstVehicle.Year);
+        }
+
+
+        public class Car
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public int Year { get; set; }
+        }
+    }
+}
